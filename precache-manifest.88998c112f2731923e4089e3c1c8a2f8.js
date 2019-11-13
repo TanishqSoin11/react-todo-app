9c665bd58c7bc3be3238881112210e9bf6f11871
TanishqSoin11 <tanishq.soin11@outllok.com>
2019-11-14 00:13:44 +0530

    Updates

---
+++ precache-manifest.88998c112f2731923e4089e3c1c8a2f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99844f62d2ce375ee960dfb1ee1c3891",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "112b4d5c3ba4e1f5ca7b",
+    "url": "/react-todo-app/static/css/main.b66e3b7a.chunk.css"
+  },
+  {
+    "revision": "5adf9e1d280372c16100",
+    "url": "/react-todo-app/static/js/2.5497b388.chunk.js"
+  },
+  {
+    "revision": "112b4d5c3ba4e1f5ca7b",
+    "url": "/react-todo-app/static/js/main.56e1e5cd.chunk.js"
+  },
+  {
+    "revision": "71bbc8a431cc44a26772",
+    "url": "/react-todo-app/static/js/runtime-main.069e21a5.js"
+  }
+]);
